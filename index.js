918b1031979eb4725b3021fa5a4dcac18aee3282
anamrouti <57967377+anamrouti@users.noreply.github.com>
2020-05-28 04:54:24 -0400

    fixing map display

--- index.js
+++ index.js
@@ -9,62 +9,81 @@ function formatQueryParams(params){
     return queryItems.join('&');
 }
 
-function getLink(query, key){
+function getLink(query, key, libraries){
     const params = {
-        query: $("#location-input").val() + ' ' + $('#address').val() ,
+        query: $('#address').val() ,
         key : api_key,
+        libraries: "places",
         
     };
 
     const queryString = formatQueryParams(params)
     const url = searchURL + '?' + queryString;
 
-    fetch(url, {mode: "no-cors"})
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    fetch(url, {mode:'no-cors'})
+    .then(response => {
+        if (response.ok){
+            return response.json();
+        }
+        //throw new error(response.statusText);
+        (response.statusText);
+    })
+    .then(responseJson => console.log(JSON.stringify(responseJson)))
+    .then(responseJson => initMap(responseJson))
     
     
     
 }
 
-function displayResults(responseJson){
-   
-    console.log(responseJson);
-
-    
-    
-    initMap();
-
-    $('#results').removeClass('hidden');
-    
-}
 
 function watchForm(){
     $('form').submit(event => {
         event.preventDefault();
-        const searchQuery = $('.location-input').val();
+        const searchQuery = $('#address').val();
         const key = api_key;
-        getLink(searchQuery, key);
-        
+        //getLink(searchQuery, key);
+        initialize();
     });
 }
-
+//var map;
+//var service;
 $(watchForm);
 
-function initMap(){
-    //const myLocation = $('.initial-location-js').val();
-    //var myLatLng = new google.maps.LatLng();
+//function initMap(results){
     
-    //var mapOptions = {
-        //center: myLatLng,
-        //zoom: 5
-    //}
     
-    //var map = new google.maps.Map(document.getElementById('map'), mapOptions) ;
+//}
 
-    
-    //}
-    //marker.setMap(map);
+var map;
+var service;
+var infowindow;
+
+function initialize() {
+  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+    console.log(map);
+    console.log(document.getElementById('map'));
+
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    query: 'restaurant'
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
 }
 
-    
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      //createMarker(results[i]);
+      console.log(place);
+    }
+  }
+}
